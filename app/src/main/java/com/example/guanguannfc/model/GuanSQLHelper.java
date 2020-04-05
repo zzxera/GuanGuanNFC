8b8e330df63424cec5d4cede5cc4c28db7b6cf1f
LxxxJ <630285616@qq.com>
2020-04-05 10:52:50 +0800

    Create GuanSQLHelper.java

---
+++ app/src/main/java/com/example/guanguannfc/model/GuanSQLHelper.java
@@ -0,0 +1,33 @@
+package com.example.guanguannfc.model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class GuanSQLHelper extends SQLiteOpenHelper {
+
+    public GuanSQLHelper(Context context){
+        super(context, GuanContract.DATABASE_NAME, null, GuanContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(GuanContract.UserInfo.SQL_CREATE_TABLE);
+        db.execSQL(GuanContract.ActivityType.SQL_CREATE_TABLE);
+        db.execSQL(GuanContract.Activity.SQL_CREATE_TABLE);
+        db.execSQL(GuanContract.ActSta.SQL_CREATE_TABLE);
+        db.execSQL(GuanContract.Box.SQL_CREATE_TABLE);
+        db.execSQL(GuanContract.BoxContent.SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL(GuanContract.UserInfo.SQL_DELETE_ENTRIES);
+        db.execSQL(GuanContract.ActivityType.SQL_DELETE_ENTRIES);
+        db.execSQL(GuanContract.Activity.SQL_DELETE_ENTRIES);
+        db.execSQL(GuanContract.ActSta.SQL_DELETE_ENTRIES);
+        db.execSQL(GuanContract.Box.SQL_DELETE_ENTRIES);
+        db.execSQL(GuanContract.BoxContent.SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+}
