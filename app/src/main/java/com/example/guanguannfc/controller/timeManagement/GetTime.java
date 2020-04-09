b9577167ee0d10acefd524c715d533826acc1cde
haitian01 <43566291+haitian01@users.noreply.github.com>
2020-04-09 19:32:12 +0800

    简单时间操作与物品管理简单方法构建

--- app/src/main/java/com/example/guanguannfc/controller/timeManagement/GetTime.java
+++ app/src/main/java/com/example/guanguannfc/controller/timeManagement/GetTime.java
@@ -1,27 +1,73 @@
 package com.example.guanguannfc.controller.timeManagement;
 
-import com.example.guanguannfc.view.data.Data;
+import android.util.Log;
 
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.text.DateFormat;
-
-//获取本地时间
+import java.text.ParseException;
+import java.util.Calendar;
+//获取本地时间、系统时间为基准
 public class GetTime {
-    java.util.Date currentdata = new java.util.Date();//当前时间
 
-    Timestamp now = new Timestamp(System.currentTimeMillis());//系统当前时间
-
-    public static String getnowDataTime(){
-        SimpleDateFormat s_format = new SimpleDateFormat("yyyyMMddhhmmss");//年月日小时分秒
-        return s_format.format(new Data());
+    //获取当前时间并显示为字符串形式 12：24：23
+    public static String getNowTime(){
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        Date curData = new Date(System.currentTimeMillis());
+        String string = format.format(curData);
+        return string;
     }
 
-    public static  String getNowTime(){
-        SimpleDateFormat s_format = new SimpleDateFormat("HH:mm:ss");//小时：分：秒
-        return s_format.format(new Data());
+    //获取起始时间并返回长整型（long）
+    public static long getStartTime(){
+        long startTime = System.currentTimeMillis();
+        return startTime;
     }
+
+    //将获取的长整型起始时间与当前的时间进行计算并返回字符串型持续时间 12：24：23
+    public static String countTime(long startTime){
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        long endTime = System.currentTimeMillis();
+        String contineTime = format.format(startTime - endTime);
+        return contineTime;
+    }
+
+    //将字符串转换为时间戳，pattern为日期格式
+    public static long getStringToDate(String dateString, String pattern) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        Date date = new Date();
+        try{
+            date = dateFormat.parse(dateString);
+        } catch(ParseException e) {
+            e.printStackTrace();
+        }
+        return date.getTime();
+    }
+
+    //Calendar应用实例
+//    public static void main(String[] args) {
+//        // 其日历字段已由当前日期和时间初始化：
+//        Calendar rightNow = Calendar.getInstance(); // 子类对象
+//        // 获取年
+//        int year = rightNow.get(Calendar.YEAR);
+//        // 获取月
+//        int month = rightNow.get(Calendar.MONTH);
+//        // 获取日
+//        int date = rightNow.get(Calendar.DATE);
+//        // 获取几点
+//        int hour=rightNow.get(Calendar.HOUR_OF_DAY);
+//        // 获取上午下午
+//        int moa=rightNow.get(Calendar.AM_PM);
+//        if(moa==1)
+//            System.out.println("下午");
+//        else
+//            System.out.println("上午");
+//        System.out.println(year + "年" + (month + 1) + "月" + date + "日"+hour+"时");
+//        rightNow.add(Calendar.YEAR,5);
+//        rightNow.add(Calendar.DATE, -10);
+//        int year1 = rightNow.get(Calendar.YEAR);
+//        int date1 = rightNow.get(Calendar.DATE);
+//        System.out.println(year1 + "年" + (month + 1) + "月" + date1 + "日"+hour+"时");
+//    }
 }
 
 
