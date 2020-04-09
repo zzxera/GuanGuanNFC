43a9321ff5a50ec9e78c618852ab7f02979d6a18
zzxera <62008030+zzxera@users.noreply.github.com>
2020-04-09 20:51:16 +0800

    Create ChangenameActivity.java

---
+++ app/src/main/java/com/example/guanguannfc/view/management/ChangenameActivity.java
@@ -0,0 +1,22 @@
+package com.example.guanguannfc.view.management;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.guanguannfc.R;
+
+public class ChangenameActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changeactname);
+    }
+
+    public void Addbox(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, AddboxActivity.class);
+        startActivity(intent);
+    }
+}
