cdecad5900279094dffad290004d1e2c42fe5db1
zzxera <62008030+zzxera@users.noreply.github.com>
2020-04-15 22:45:25 +0800

    Update TimemanagementActivity.java

--- app/src/main/java/com/example/guanguannfc/view/management/TimemanagementActivity.java
+++ app/src/main/java/com/example/guanguannfc/view/management/TimemanagementActivity.java
@@ -3,6 +3,7 @@ package com.example.guanguannfc.view.management;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
