74d3ca1a8883e2538c89e0f45d1f8d462266c28c
zzxera <62008030+zzxera@users.noreply.github.com>
2020-04-15 22:45:19 +0800

    Update SearchgoodsActivity.java

--- app/src/main/java/com/example/guanguannfc/view/management/SearchgoodsActivity.java
+++ app/src/main/java/com/example/guanguannfc/view/management/SearchgoodsActivity.java
@@ -3,20 +3,39 @@ package com.example.guanguannfc.view.management;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.guanguannfc.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SearchgoodsActivity extends AppCompatActivity {
+    private ListView listView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchgoods);
+        listView = (ListView) findViewById(R.id.listview);
+        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this, this.getData(),
+                R.layout.activity_listview2,
+                new String[]{"tvName","tv_shuliang"},
+                new int[]{R.id.tvName,R.id.tv_shuliang});
+        listView.setAdapter(mSimpleAdapter);
     }
+    private List<Map<String,Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String ,Object>>();
+        String [] name=new String[]{"新的朋友","群聊","标签","公众号"};
+        for (int i=0;i<name.length;i++)
+        {
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("tvName",name[i]);
+            map.put("tv_shuliang",name[i]);
+            list.add(map);
+        }
+        return list;
 
-    public void Displaygoods(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplaygoodsActivity.class);
-        startActivity(intent);
     }
 }
