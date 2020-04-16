35ebb79eabcd33a078ae0f245203c04a97fb7f85
cheristy123 <44560275+cheristy123@users.noreply.github.com>
2020-04-16 10:08:20 +0800

    controller第一次修改

--- app/src/main/java/com/example/guanguannfc/controller/userManagement/Register.java
+++ app/src/main/java/com/example/guanguannfc/controller/userManagement/Register.java
@@ -1,5 +1,6 @@
 package com.example.guanguannfc.controller.userManagement;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.text.TextUtils;
 
@@ -8,8 +9,11 @@ import com.example.guanguannfc.model.Dao.DaoUserInfo;
 
 import static android.content.Context.MODE_PRIVATE;
 
-public class Register extends AppCompatActivity {
-    DaoUserInfo DD = new DaoUserInfo(this);
+public class Register {
+    DaoUserInfo DD;
+    public Register(Context context){
+        this.DD = new DaoUserInfo(context);
+    }
     public boolean RisExistUserName(String userName) {
         boolean hasUserName = DD.registrationQuery(userName);
         return hasUserName;
