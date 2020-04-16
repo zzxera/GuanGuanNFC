5cfd41c81990ea495b59e9cf0dbc866c54769bd4
zzxera <62008030+zzxera@users.noreply.github.com>
2020-04-16 17:03:41 +0800

    Update DisplaygoodsActivity.java

--- app/src/main/java/com/example/guanguannfc/view/management/DisplaygoodsActivity.java
+++ app/src/main/java/com/example/guanguannfc/view/management/DisplaygoodsActivity.java
@@ -25,23 +25,6 @@ public class DisplaygoodsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_displaygoods);
-        listView = (ListView) findViewById(R.id.listview);
-        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this, this.getData(),
-                R.layout.activity_listview1,
-                new String[]{"tv_Name","btn_delet"},
-                new int[]{R.id.tv_name});
-        listView.setAdapter(mSimpleAdapter);
-    }
-
-    private List<? extends Map<String,?>> getData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        Map<String,Object> map=new HashMap<String, Object>();
-        map.put("tv_name", "Apple");
-        list.add(map);
-        map.put("tv_name", "Appe");
-        list.add(map);
-        return list;
-
     }
 
 }
