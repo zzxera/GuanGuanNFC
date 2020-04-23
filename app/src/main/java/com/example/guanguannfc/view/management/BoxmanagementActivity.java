81531207005346702bc8f70709d4f756357961bd
zzxera <62008030+zzxera@users.noreply.github.com>
2020-04-23 15:53:56 +0800

    Update BoxmanagementActivity.java

--- app/src/main/java/com/example/guanguannfc/view/management/BoxmanagementActivity.java
+++ app/src/main/java/com/example/guanguannfc/view/management/BoxmanagementActivity.java
@@ -2,17 +2,21 @@ package com.example.guanguannfc.view.management;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.guanguannfc.R;
+import com.example.guanguannfc.controller.dataManagement.ThingManage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,26 +26,106 @@ import java.util.Map;
 public class BoxmanagementActivity extends AppCompatActivity {
 
     private PopupWindow mPopWindow;
+    private String username;
+    private ThingManage boxget =new ThingManage(username,this);
+    private String[][] box;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boxmanagement);
-        Button btn1 =findViewById(R.id.Button1);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        box=boxget.boxAndPosition();
+        Bundle bundle = this.getIntent().getExtras();
+        ImageView iv_box1 =findViewById(R.id.iv_box1);
+        iv_box1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showPopupWindow();
+                if(box[0][0]!=null){
+                    showPopupWindow();
+                }
+                else {
+                    showPopupWindow4();
+                }
             }
         });
-        Button btn3 = findViewById(R.id.Button1);
-        btn3.setOnLongClickListener(new View.OnLongClickListener() {
+        ImageView iv_box_1= findViewById(R.id.iv_box1);
+        iv_box_1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 showPopupWindow3();
                 return false;
             }
         });
-        Button btn4 = findViewById(R.id.btn_addbox);
-        btn4.setOnClickListener(new View.OnClickListener() {
+
+        ImageView iv_box2 =findViewById(R.id.iv_box2);
+        iv_box2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(box[0][1]!=null){
+                    showPopupWindow5();
+                }
+                else {
+                    showPopupWindow4();
+                }
+            }
+        });
+        ImageView iv_box3 =findViewById(R.id.iv_box3);
+        iv_box3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(box[0][2]!=null){
+                    showPopupWindow();
+                }
+                else {
+                    showPopupWindow4();
+                }
+            }
+        });
+
+        ImageView iv_box4 =findViewById(R.id.iv_box4);
+        iv_box4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(box[0][3]!=null){
+                    showPopupWindow();
+                }
+                else {
+                    showPopupWindow4();
+                }
+            }
+        });
+
+        ImageView iv_box5 =findViewById(R.id.iv_box5);
+        iv_box5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(box[0][4]!=null){
+                    showPopupWindow();
+                }
+                else {
+                    showPopupWindow4();
+                }
+            }
+        });
+
+        ImageView iv_box6 =findViewById(R.id.iv_box6);
+        iv_box6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(box[0][5]!=null){
+                    showPopupWindow();
+                }
+                else {
+                    showPopupWindow4();
+                }
+            }
+        });
+
+
+
+
+
+
+        ImageView addbox = findViewById(R.id.iv_addbox);
+        addbox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showPopupWindow4();
@@ -87,32 +171,100 @@ public class BoxmanagementActivity extends AppCompatActivity {
         });
 
         ListView listView =contentView.findViewById(R.id.listview);
-        SimpleAdapter mSimpleAdapter2 = new SimpleAdapter(this, this.getData(),
+        TextView tv_boxname=contentView.findViewById(R.id.tv_boxname);
+        tv_boxname.setText(box[0][0]);
+        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this, this.getData1(),
                 R.layout.activity_listview3,
                 new String[]{"tv_goods_name","tv_goods_shuliang","btn_change_num"},
                 new int[]{R.id.tv_goods_name,R.id.tv_goods_shuliang,R.id.btn_change_num});
-        listView.setAdapter(mSimpleAdapter2);
+        listView.setAdapter(mSimpleAdapter);
         //显示PopupWindow
         View rootview = LayoutInflater.from(BoxmanagementActivity.this).inflate(R.layout.activity_boxmanagement, null);
         mPopWindow.showAtLocation(rootview, Gravity.CENTER, 0, 0);
     }
 
-    private List<Map<String,Object>> getData() {
+    private void showPopupWindow5() {
+        //设置contentView
+        View contentView = LayoutInflater.from(BoxmanagementActivity.this).inflate(R.layout.activity_displaygoods, null);
+        mPopWindow = new PopupWindow(contentView, ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT, true);
+        //设置各个控件的点击响应
+        Button btn2 =contentView.findViewById(R.id.btb_addgoods);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupWindow2();
+            }
+        });
+
+        ListView listView =contentView.findViewById(R.id.listview);
+        TextView tv_boxname=contentView.findViewById(R.id.tv_boxname);
+        tv_boxname.setText(box[0][1]);
+        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this, this.getData2(),
+                R.layout.activity_listview3,
+                new String[]{"tv_goods_name","tv_goods_shuliang","btn_change_num"},
+                new int[]{R.id.tv_goods_name,R.id.tv_goods_shuliang,R.id.btn_change_num});
+        listView.setAdapter(mSimpleAdapter);
+        //显示PopupWindow
+        View rootview = LayoutInflater.from(BoxmanagementActivity.this).inflate(R.layout.activity_boxmanagement, null);
+        mPopWindow.showAtLocation(rootview, Gravity.CENTER, 0, 0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private List<Map<String,Object>> getData1() {
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        String [] name=new String[]{"化妆1","化妆2","化妆3","化妆4"};
-        String [] num = new String[]{"2","5","4","7"};
-        String [] num2 = new String[]{"2","5","4","7"};
-        for (int i=0;i<name.length;i++)
+        String [] name;
+        String [] num;
+        String [][] thing=boxget.thingAndNumberInBox(box[0][0]);
+        name=thing[0];
+        num=thing[1];
+        for (int j=0;j<name.length;j++)
         {
             Map<String,Object> map=new HashMap<String, Object>();
-            map.put("tv_goods_name",name[i]);
-            map.put("tv_goods_shuliang",num[i]);
+            map.put("tv_goods_name",name[j]);
+            map.put("tv_goods_shuliang",num[j]);
             map.put("btn_change_num",R.id.btn_change_num);
             list.add(map);
         }
-
         return list;
     }
+
+    private List<Map<String,Object>> getData2() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        String [] name;
+        String [] num;
+        String [][] thing=boxget.thingAndNumberInBox(box[0][1]);
+        name=thing[0];
+        num=thing[1];
+        for (int j=0;j<name.length;j++)
+        {
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("tv_goods_name",name[j]);
+            map.put("tv_goods_shuliang",num[j]);
+            map.put("btn_change_num",R.id.btn_change_num);
+            list.add(map);
+        }
+        return list;
+    }
+
+
+
+
+
+
+
+
+
+
 
     private void showPopupWindow2(){
         View contentView = LayoutInflater.from(BoxmanagementActivity.this).inflate(R.layout.activity_addgoods, null);
