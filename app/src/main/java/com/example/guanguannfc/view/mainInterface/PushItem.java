571b3617f7a14c4b3f08d9969e11e2a2e498287e
zzxera <62008030+zzxera@users.noreply.github.com>
2020-05-28 14:27:59 +0800

    Update PushItem.java

--- app/src/main/java/com/example/guanguannfc/view/mainInterface/PushItem.java
+++ app/src/main/java/com/example/guanguannfc/view/mainInterface/PushItem.java
@@ -5,10 +5,11 @@ public class PushItem {
     private String title,author,content;
     private int imageId;
 
-    public PushItem(String[] array){
+    public PushItem(String[] array, String s){
         this.title=array[1];
         this.author=array[0];
         this.content=array[2];
+        this.imageId=Integer.parseInt(s);
         //this.imageId=Integer.parseInt(array[3]);
     }
     public String getTitle(){
@@ -20,7 +21,5 @@ public class PushItem {
     public String getContent(){
         return content;
     }
-    //public int getImageId(){
-        //return imageId;
-   // }
+    public int getImageId(){ return imageId; }
 }
