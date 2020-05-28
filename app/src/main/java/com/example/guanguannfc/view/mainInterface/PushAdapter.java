281cc24bb7e58407a6d972d7ca1cc21f0a11e803
zzxera <62008030+zzxera@users.noreply.github.com>
2020-05-28 14:05:00 +0800

    Update PushAdapter.java

--- app/src/main/java/com/example/guanguannfc/view/mainInterface/PushAdapter.java
+++ app/src/main/java/com/example/guanguannfc/view/mainInterface/PushAdapter.java
@@ -1,11 +1,15 @@
 package com.example.guanguannfc.view.mainInterface;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,9 +18,13 @@ import androidx.annotation.Nullable;
 import com.example.guanguannfc.R;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PushAdapter extends ArrayAdapter {
     private final int resourceId;
+    private PopupWindow mPopWindow;
+    private Objects img;
+    private List<Object> list;
 
     public PushAdapter(Context context, int textViewResourceId, List<PushItem> objects) {
         super(context, textViewResourceId, objects);
@@ -32,13 +40,27 @@ public class PushAdapter extends ArrayAdapter {
         TextView tv_title = view.findViewById(R.id.tv_title);
         TextView tv_author = view.findViewById(R.id.tv_author);
         TextView tv_content = view.findViewById(R.id.tv_content);
-
-        imageView.setImageResource(pushItem.getImageId());
+        //imageView.setImageResource(pushItem.getImageId());
+        imageView.setImageDrawable((Drawable) list.get(position));
+        imageView.setImageResource();
         tv_title.setText(pushItem.getTitle());
         tv_author.setText(pushItem.getAuthor());
         tv_content.setText(pushItem.getContent());
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showpush();
+            }
+        });
 
 
         return view;
+    }
+    private void showpush(){
+        View contentView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_push_show, null);
+        mPopWindow = new PopupWindow(contentView, ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT, true);
+
+        View rootview = LayoutInflater.from(getContext()).inflate(R.layout.fragment_push,null);
+        mPopWindow.showAtLocation(rootview, Gravity.CENTER, 0, 0);
     }
 }
