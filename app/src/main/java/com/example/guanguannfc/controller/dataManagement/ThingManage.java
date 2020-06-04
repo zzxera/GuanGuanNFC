4ea9c249cd5fc2ce0e1891e743cf398927836074
haitian01 <43566291+haitian01@users.noreply.github.com>
2020-06-04 14:32:11 +0800

    搜索物品方法完毕
    返回一个字符串类型的数组

--- app/src/main/java/com/example/guanguannfc/controller/dataManagement/ThingManage.java
+++ app/src/main/java/com/example/guanguannfc/controller/dataManagement/ThingManage.java
@@ -1,11 +1,8 @@
 package com.example.guanguannfc.controller.dataManagement;
 
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
 
 import com.example.guanguannfc.model.Dao.DaoBox;
-import com.example.guanguannfc.model.Dao.DaoActivityType;
 import com.example.guanguannfc.model.Dao.DaoBoxContent;
 import com.example.guanguannfc.model.Helper.HelperBox;
 import com.example.guanguannfc.model.Helper.HelperBoxContent;
@@ -27,11 +24,19 @@ public class ThingManage {
         mDaoBoxContent = new DaoBoxContent(context);
     }
 
-    //搜索物品
-    public String[] searchThing(String thingName){
-        String[] boxlist = new String[2];
-        return boxlist;
+    //搜索物品,返回盒子集合
+    public String[] searchThing(String thingname){
+        ArrayList list = daoBox.queryBox(username, thingname);
+        if (list  == null){
+            return null;
+        }
+        String[] array = new String[list.size()];
+        for (int i = 0; i< list.size(); i++){
+            array[i] = (String) list.get(i);
+        }
+        return array;
     }
+
     //张浦鑫需要的
     public String[][] thingAndNumberInBox(String boxName){
         HashMap<String, ArrayList<HelperBoxContent>> hashMap = new HashMap<String, ArrayList<HelperBoxContent>>();
