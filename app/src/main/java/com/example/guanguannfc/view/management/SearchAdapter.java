5b897b46591d1880ac5f8293de5c79e683fa8728
zzxera <62008030+zzxera@users.noreply.github.com>
2020-06-04 14:42:56 +0800

    Update SearchAdapter.java

--- app/src/main/java/com/example/guanguannfc/view/management/SearchAdapter.java
+++ app/src/main/java/com/example/guanguannfc/view/management/SearchAdapter.java
@@ -1,9 +1,18 @@
 package com.example.guanguannfc.view.management;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.example.guanguannfc.R;
 
 public class SearchAdapter extends BaseAdapter {
     private Context context;
@@ -28,6 +37,22 @@ public class SearchAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        final ViewHolder ViewHolder;
+        if (convertView==null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.searchlist,parent,false);
+            ViewHolder = new ViewHolder();
+            ViewHolder.tv_sv_boxname = (TextView)convertView.findViewById(R.id.tv_sv_boxname);
+            convertView.setTag(ViewHolder);
+
+        }else {
+            ViewHolder = (ViewHolder) convertView.getTag();
+        }
+        ViewHolder.tv_goods_name.setText(goodsname[position]);
+        ViewHolder.tv_goods_shuliang.setText(goodsnum[position]);
+        return convertView;
     }
+    static class ViewHolder {
+        TextView tv_sv_boxname;
+    }
+
 }
