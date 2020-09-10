16c7ccf9dd62575a5e007b975583177a6767b8d7
zzxera <62008030+zzxera@users.noreply.github.com>
2020-09-10 09:52:04 +0800

    Update FriendRequestAdapter.java

--- app/src/main/java/com/example/guanguannfc/view/friends/FriendRequestAdapter.java
+++ app/src/main/java/com/example/guanguannfc/view/friends/FriendRequestAdapter.java
@@ -1,11 +1,14 @@
 package com.example.guanguannfc.view.friends;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,6 +16,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.guanguannfc.R;
+import com.example.guanguannfc.model.util.HttpUtil;
+import com.example.guanguannfc.model.util.ImgNameUtil;
 
 import org.w3c.dom.Text;
 
@@ -21,23 +26,41 @@ import java.util.List;
 public class FriendRequestAdapter extends ArrayAdapter {
     private final int resourceId;
     Button refuse,agree;
+    private Activity activity;
+    private Drawable drawable;
 
-    public FriendRequestAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<FriendRequestItem> objects) {
+    public FriendRequestAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<FriendRequestItem> objects, Activity activity) {
         super(context, textViewResourceId, objects);
         resourceId = textViewResourceId;
+        this.activity = activity;
     }
 
     @NonNull
     @Override
     public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        FriendRequestItem friendRequestItem = (FriendRequestItem) getItem(position);
+        final FriendRequestItem friendRequestItem = (FriendRequestItem) getItem(position);
         View view =  LayoutInflater.from(getContext()).inflate(resourceId, null);
         TextView name = view.findViewById(R.id.tv_friendName);
         final TextView remark = view.findViewById(R.id.tv_remark);
+        final ImageView imageView = view.findViewById(R.id.img_head);
         refuse = view.findViewById(R.id.btn_refuse);
         agree = view.findViewById(R.id.btn_agree);
 
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                drawable = HttpUtil.getImg(ImgNameUtil.getImgHeadName(friendRequestItem.getName()));
+                if (drawable != null) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageDrawable(drawable);
+                        }
+                    });
+                }
+            }
+        }).start();
+        imageView.setImageResource(R.drawable.img_head);
         name.setText(friendRequestItem.getName());
         remark.setText(friendRequestItem.getRemark());
         refuse.setOnClickListener(new View.OnClickListener() {
